dc50c90db91e15de562b8fa5d17162988648df66
Niyamaka <niyamaka@yuneta.io>
2020-05-10 11:14:21 +0200

    ajusta fit in 390px

--- src/yuno.js
+++ src/yuno.js
@@ -34,6 +34,7 @@
      ************************************************************/
     var CONFIG = {
         tracing: 0,
+        no_poll: 0,
         trace_timer: 0,
         trace_inter_event: false,
         trace_creation: false,
